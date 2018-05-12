9d979aed73eb4dac970109e8d6db6d47a4d9e8ed
marco-esposito <m.espo@tiscali.it>
2018-05-12 12:23:45 +0200

    Revert "babel eslint+airbnb, config ignored by git, webpack modified"
    This reverts commit 44ac8038d6e79e8e159b4bf94f748427f7152f10.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  devtool: 'source-map',
   entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
@@ -12,20 +11,20 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015', 'stage-1'],
-        },
-      },
-    ],
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000,
-    },
-  },
+      poll: 1000
+    }
+  }
 };
